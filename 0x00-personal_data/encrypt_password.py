8e159ab4cfb2effb2070494f1c40f17c62addebe
glackyy <othmanesticky@gmail.com>
2024-02-08 09:05:07 +0100

    encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Defining a hash_password to return a hashed password"""
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """Returning a hashed password"""
+    bc = password.encode()
+    hashed = hashpw(bc, bcrypt.gensalt())
+    return hashed
+
