bec724da1a4282c3d3c9c0d1097c677c4a174108
glackyy <othmanesticky@gmail.com>
2024-02-08 08:46:25 +0100

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,3 +63,21 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                                   host=host,
                                   database=db_name)
     return con
+
+
+def main():
+    """Main entry point"""
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={};".format(k, val) for k, val in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
