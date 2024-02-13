4b0dd9b96613bbaa772e9a32c0dbebe9ff12e49d
glackyy <othmanesticky@gmail.com>
2024-02-13 10:19:10 +0100

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Auth Class"""
+from flask import request
+from typing import (
+    List,
+    TypeVar
+)
+
+
+class Auth:
+    """Managing the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determining whether a given path requires auth or not"""
+        if path is None:
+            return True
