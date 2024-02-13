e2ab7ac83620c3641e810abfa955e3f51ea7c32a
glackyy <othmanesticky@gmail.com>
2024-02-13 10:41:51 +0100

    basicauth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,3 +48,34 @@ class BasicAuth(Auth):
         email = decoded_base64_authorization_header.split(":")[0]
         password = decoded_base64_authorization_header[len(email) + 1:]
         return (email, password)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returning a user instance based on email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for us in users:
+                if us.is_valid_password(user_pwd):
+                    return us
+            return None
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returning a user instance based on a received request"""
+        auth_header = self.authorization_header(request)
+        if auth_header is not None:
+            tk = self.extract_base64_authorization_header(auth_header)
+            if tk is not None:
+                dd = self.decode_base64_authorization_header(tk)
+                if dd is not None:
+                    email, passwd = self.extract_user_credentials(dd)
+                    if email is not None:
+                        return self.user_object_from_credentials(email, passwd)
+        return
