cabce8df412c4e7dfab2ecc375c4922235b744c9
glackyy <othmanesticky@gmail.com>
2024-02-13 09:51:30 +0100

    app.py, index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -15,3 +15,8 @@ def authorized() -> str:
     """Getting /api/v1/unauthorized"""
     abort(401, description="Unauthorized")
 
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbid() -> str:
+    """Getting /api/v1/forbidden"""
+    abort(403, description="Forbidden")
