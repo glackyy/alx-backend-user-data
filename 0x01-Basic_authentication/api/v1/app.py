24070205ee6ac4b3c02dae1ed26a5c495eaf0c53
glackyy <othmanesticky@gmail.com>
2024-02-13 08:49:59 +0100

    adding files

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Route module for the API"""
+from os import getenv
+from flask import Flask
+
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
