ae99ed9448f35fb3a91e2a9446c9cea1e3559f12
glackyy <othmanesticky@gmail.com>
2024-02-13 09:06:53 +0100

    hot fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 """Route module for the API"""
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
+
+
+@app.route('/api/v1/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
