3be495f9e74317c0dc3805d96119024fcae43f7b
glackyy <othmanesticky@gmail.com>
2024-02-15 09:18:13 +0100

    SDBA

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""class SessionDBauth"""
+from .session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """Defining a SessionDBAuth class that persists
+    session data in db"""
+    def create_session(self, user_id: None):
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        kwargs = {
+            "user_id": user_id,
+            "session_id": session_id
+        }
+        user = UserSession(**kwargs)
+        user.save()
+        return session_id
