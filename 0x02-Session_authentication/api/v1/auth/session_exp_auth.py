2a039e5d03611f68a396ac1f37d0aff3a7b45ab3
glackyy <othmanesticky@gmail.com>
2024-02-15 09:06:31 +0100

    session exp auth

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -2,6 +2,8 @@
 """SessionExpAuth class"""
 from .session_auth import SessionAuth
 import os
+from datetime import datetime
+
 
 class SessionExpAuth(SessionAuth):
     """Defining class SEA that adds an
@@ -13,3 +15,15 @@ class SessionExpAuth(SessionAuth):
         except Exception:
             duration = 0
         self.session_duration = duration
+
+    def create_session(self, user_id: None):
+        """Creating a session id for user"""
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dict = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+        self.user_id_by_session_id[session_id] = session_dict
+        return session_id
