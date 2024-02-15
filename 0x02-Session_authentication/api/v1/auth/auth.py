982434a31f7995e678a834a95f7122bc2a9527d8
glackyy <othmanesticky@gmail.com>
2024-02-15 07:32:41 +0100

    adding api + models

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python3
+"""Auth Class"""
+from flask import request
+from typing import (
+    List,
+    TypeVar
+)
+
+
+class Auth:
+    """Managing the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determining whether a given path requires auth or not"""
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Returning the Auth header from a request obj"""
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returning a User instance from info from a request obj"""
+        return None
