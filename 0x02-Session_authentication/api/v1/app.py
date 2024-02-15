1696cc8644f00ca19e04002d675bc2ad9ab3dacb
glackyy <othmanesticky@gmail.com>
2024-02-15 08:11:15 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,10 +38,12 @@ def before_req():
         excluded = [
             '/api/v1/status',
             '/api/v1/unauthorized',
-            '/api/v1/forbidden'
+            '/api/v1/forbidden',
+            '/api/v1/auth_session/login'
         ]
         if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
+            ck = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and ck is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
