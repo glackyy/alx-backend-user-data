6158baf96f2ace9629f80fbaf447dbfa65f64a20
glackyy <othmanesticky@gmail.com>
2024-02-15 09:04:13 +0100

    session exp auth

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""SessionExpAuth class"""
+from .session_auth import SessionAuth
+import os
+
+class SessionExpAuth(SessionAuth):
+    """Defining class SEA that adds an
+    exp date to a session id"""
+    def __init__(self):
+        """Initializing the class"""
+        try:
+            duration = int(os.getenv('SESSION_DURATION'))
+        except Exception:
+            duration = 0
+        self.session_duration = duration
