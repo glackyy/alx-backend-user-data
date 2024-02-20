1954a8617bb1abfb5c7a25c8aacb3ac362e9e90b
glackyy <othmanesticky@gmail.com>
2024-02-20 07:20:55 +0100

    user.py

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Declaring a SQLAlchemy user corresponding to db "users" """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
+
+Base = declarative_base()
