b1a247db99cf1b8ccd019b443d8fb302c6a9f987
glackyy <othmanesticky@gmail.com>
2024-02-20 08:15:38 +0100

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""_hash_password function"""
+import bcrypt
+
+
+def _hash_password(password:str) -> bytes:
+    """Hashing a passwd str and returning it in bytes"""
+    passwd = password.encode('utf-8')
+    return bcrypt.hashpw(passwd, bcrypt.gensalt())
