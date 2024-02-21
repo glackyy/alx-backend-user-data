b174b1ae6a5435c92e7b7fc86845db442b3c05a1
glackyy <othmanesticky@gmail.com>
2024-02-21 14:37:42 +0100

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Flask App"""
+from flask import (
+    Flask,
+    jsonify
+)
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """Returning json response"""
+    return jsonify({"message": "Bienvenue"})
