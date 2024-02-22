dcfde8f3257489c322e932dc26034103dc24bb17
glackyy <othmanesticky@gmail.com>
2024-02-22 07:54:39 +0100

    main

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Main File"""
+import requests
+
+
+def register_user(email: str, password: str) -> None:
+    """Testing register a usr with given email and password"""
+    res = requests.post('http://127.0.0.1:5000/users',
+                        data={'email': email, 'password': password})
+    if res.status_code == 200:
+        assert (res.json() == {"email": email, "message": "user created"})
+    else:
+        assert(res.status_code == 400)
+        assert (res.json() == {"message": "email already registered"})
